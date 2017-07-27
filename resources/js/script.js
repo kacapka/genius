1d7bec43b91e815e7428004f185ab87d300aeeb4
kacapka <wojciech.urbansk@gmail.com>
2017-07-27 21:39:33 +0200

    s

--- resources/js/script.js
+++ resources/js/script.js
@@ -468,7 +468,7 @@
 //    pricingBox.forEach(function(b) {
 //        b.addEventListener('click', activePricingBox)    
 //    });
-        $('.pricing_box').click(activePricingBox);
+        $('.pricing_box').on('click', activePricingBox);
 	
 	var data = [
 		{
