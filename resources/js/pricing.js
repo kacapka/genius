0511b5da1adadbf67fd97a372cb561bd12d69e53
kacapka <wojciech.urbansk@gmail.com>
2017-07-28 17:32:55 +0200

    s

--- resources/js/pricing.js
+++ resources/js/pricing.js
@@ -25,18 +25,19 @@
                 
                 var li = document.createElement('li');
                 li.classList.add('row', 'mobile-pricing-details');
-                li.innerHTML = `
-                    <div class="row">
-                        <span class="price"><span class="price_amount">vvv</span>zl <span class="price_option">  /normal</span></span>
-                        <span class="price"><span class="price_amount_2">ccc</span>zl <span class="price_option">  /student</span></span>
-                    </div>
-                            
-                    <div class="row">
-                        <p class="price_description">
-                            xxx
-                        </p>
-                    </div>
-                `  
+               li.innerHTML = 'hahaha'; 
+                   //`
+//                    <div class="row">
+//                        <span class="price"><span class="price_amount">vvv</span>zl <span class="price_option">  /normal</span></span>
+//                        <span class="price"><span class="price_amount_2">ccc</span>zl <span class="price_option">  /student</span></span>
+//                    </div>
+//                            
+//                    <div class="row">
+//                        <p class="price_description">
+//                            xxx
+//                        </p>
+//                    </div>
+//                `  
                 this.after(li);
                 activeDataItem.isOpen = true;
             }
